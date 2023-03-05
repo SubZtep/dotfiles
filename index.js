029a36b824f3ff6bc77761521eecbf6ced9f3a10
Andras Serfozo <subztep@gmail.com>
2023-03-05 19:24:25 +0000

    Extends eslint:recommended (test)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = {
   configs: {
     recommended: {
       plugins: ["subztep"],
+      env: ["browser"],
+      extends: ["eslint:recommended"],
       rules: {
         "no-constant-condition": "warn",
       },
