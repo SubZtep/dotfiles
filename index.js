50974f244a61ccbbabbd0aa04da11a5a4aa09028
Andras Serfozo <subztep@gmail.com>
2023-03-05 19:28:08 +0000

    Add es6 to env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   configs: {
     recommended: {
       plugins: ["subztep"],
-      env: ["browser"],
+      env: ["browser", "es6"],
       extends: ["eslint:recommended"],
       rules: {
         "no-constant-condition": "warn",
